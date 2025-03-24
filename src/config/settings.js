330919f4dc8ea10963fa882078626966fb2c75a0
Leonardo <neves.leonardoc@gmail.com>
2025-03-23 21:18:08 -0300

    first test

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,6 +1,6 @@
 export default {
 	title: `Ada Robotics | `,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
+	description: `Bem vindo!`,
 	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
 	name: `Ada Robotics`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: false,
